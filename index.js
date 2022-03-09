ab2c2faf3729c7e00a6308939bb2c42a95b9bb87
bluuweb <contacto@bluuweb.cl>
2022-03-09 13:07:11 -0300

    session v2

--- index.js
+++ index.js
@@ -16,7 +16,18 @@ require("./database/db");
 
 const app = express();
 
-app.use(cors());
+const whitelist = ["http://localhost:5000", ["https://uur.herokuapp.com/"]];
+const corsOptions = {
+    credentials: true,
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+};
+app.use(cors(corsOptions));
 
 app.use(
     session({
